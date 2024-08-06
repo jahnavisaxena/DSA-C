59396f74b5e87140ea0eded8be8051d85619375f
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-06 23:55:19 +0530

    Create Q5.c

---
+++ Patterns/Q5.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+void rhombus(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int j=1;j<=n;j++){
+            printf("*");
+        }
+       printf("\n"); 
+    }
+    
+}
+void main(){
+    rhombus(5);
+}
