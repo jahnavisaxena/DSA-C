d191911e54d5a348310f15a5aeac3eae39304bfa
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-06 23:56:50 +0530

    Create Q10.c

---
+++ Patterns/Q10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void hollowSquare(int n)
+{
+ for(int i=1;i<=n;i++)
+ {
+    for(int j=1;j<=n;j++)
+    {
+        if(i== 1||i==5 || j == 1 || j == 5)
+        {
+            printf("*");
+        }
+        else{
+            printf(" ");
+        }
+        
+    }
+    printf("\n");
+ }
+}
+void main()
+{
+    hollowSquare(5);
+}
