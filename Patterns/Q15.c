833c18b889ab44c11feab790f8da03017f71aa17
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-07 23:23:07 +0530

    Create Q15.c

---
+++ Patterns/Q15.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void fifthPattern(int n)
+{
+    for(int i=1;i<=n;i++)
+ {
+    for(int j=1;j<=n;j++)
+    {
+        if(i== 1||i==n || j == 1 || j == n)
+        {
+            printf("* ");
+        }
+        else{
+            printf("%d ",j);
+        }
+        
+    }
+    printf("\n");
+ }
+}
+void main()
+{
+    fifthPattern(5);
+}
